d29b31437eda9ea0a48a19a3bfe91cad18eaf934
Danylo Lazorenko <37270707+Souldanka@users.noreply.github.com>
2024-01-22 19:54:44 +0200

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel();
-  });$ npm install swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    390:{
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
+});
